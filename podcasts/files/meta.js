84b179b2ab81b0d5e79846a130369e2a7291176a
Vitalii <vitaliio@steps.me>
2023-05-12 11:07:47 +0300

    podcast s1e1

---
+++ podcasts/files/meta.js
@@ -0,0 +1,74 @@
+(function() {
+	var Realmac = Realmac || {};
+
+	Realmac.meta = {
+		
+		// Set the browser title
+		//
+		// @var String text
+		setTitle: function(text) {
+			return document.title = text;
+		},
+		
+		// Set the content attribute of a meta tag
+		//
+		// @var String name
+		// @var String content
+		setTagContent: function(tag, content){
+			// If the tag being set is title
+			// return the result of setTitle
+			if ( tag === 'title' )
+			{
+				return this.setTitle(content);
+			}
+			
+			// Otherwise try and find the meta tag
+			var tag = this.getTag(tag);
+			
+			// If we have a tag, set the content
+			if ( tag !== false )
+			{
+				return tag.setAttribute('content', content);
+			}
+			
+			return false;
+		},
+		
+		// Find a meta tag
+		//
+		// @var String name
+		getTag: function(name) {
+			var meta = document.querySelectorAll('meta');
+			
+			for ( var i=0; i<meta.length; i++ )
+			{
+				if (meta[i].name == name){
+					return meta[i];
+				}
+			}
+			
+			var tag = document.createElement('meta');
+			tag.name = name;
+			document.getElementsByTagName('head')[0].appendChild(tag);
+			
+			return tag;
+		}
+	};
+ 
+	// Object containing all website meta info
+	var websiteMeta = {"archive-may-2023.html":"Archives for May 2023","1deec6b5a058f9fc38e59404526d6cfc-0.html":"#Теми подкаста\n\n1.\tFoMo - як з цим жити \/ як побороти \n2.\tЕмоційне вигорання на роботі \n\n> Podcast"};
+ 
+	// pageId must match the key in websiteMeta object
+	var url = window.location.pathname;
+	var pageId = url.substring(url.lastIndexOf('/')+1);
+	if (!pageId || pageId.length == 0){
+		pageId = 'index.html';
+	}
+	pageMeta = websiteMeta[pageId];
+ 
+	// If we have meta for this page
+	if (pageMeta){
+		Realmac.meta.setTagContent('description', pageMeta);
+	}
+ 
+ })();
